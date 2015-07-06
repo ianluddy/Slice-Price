516c07cff00ebb12e0600c94d2aedd83be7c745e
Ian Luddy <ianluddy@gmail.com>
2015-07-06 12:30:25 +0100

    added some utils

--- slice_scanner/utils.py
+++ slice_scanner/utils.py
@@ -1,3 +1,4 @@
+from flask import make_response
 from uuid import uuid5, NAMESPACE_DNS
 from logging.handlers import RotatingFileHandler
 import logging
@@ -16,6 +17,18 @@ def wrapped_execute(func, *args, **kwargs):
         return func(*args, **kwargs)
     except Exception, e:
         logging.error("Fatal error calling %s" % str(func), exc_info=True)
+
+def raw_response(response_string):
+    response = make_response(response_string)
+    response.mimetype = "text/plain"
+    return response
+
+def json_response(response):
+    if type(response) is dict:
+        response = json.dumps(response)
+    response = make_response(response)
+    response.mimetype = "application/json"
+    return response
 
 def setup_logger(app, log_file, log_level):
     logger = logging.getLogger()
