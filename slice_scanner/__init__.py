1f3926b912b5dcb38ea9c36e7b12f06f84773631
ian luddy <ianluddy@gmail.com>
2017-10-29 16:34:28 +0000

    docs

--- slice_scanner/__init__.py
+++ slice_scanner/__init__.py
@@ -9,7 +9,7 @@ from database import Database
 from collector import Collector
 from cleaner import Cleaner
 from keeper import Keeper
-from flask.ext.autodoc import Autodoc
+from flask_autodoc import Autodoc
 from flask_pymongo import MongoClient
 from flask import Flask
 from utils import setup_logger, read_config_file
